518e0db6535fb2bb213a330ab66599bf6710a632
biplab <biplabmal626@gmail.com>
2024-12-23 00:30:05 +0530

    change

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+import { Sidebar } from "@/components/Sidebar";
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Sidebar />
+        {children}
+      </body>
+    </html>
+  );
+}
